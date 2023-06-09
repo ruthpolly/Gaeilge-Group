f09b4012178fad441408641b52a0b1dc6ec79432
ruthpolly <ruthpollyosull@gmail.com>
2023-06-09 22:07:34 +0000

    Add instructions for quiz

--- run.py
+++ run.py
@@ -26,15 +26,23 @@ def enter_username():
         print("Name is required to begin.")
         enter_username()
     else:
-        start_quiz()	
+        start_quiz()
+
 
 def start_quiz():
     """
-    Provides instructions and loads quiz if user agrees, or else returns to 
+    Provides instructions and loads quiz if user agrees, or else returns to
     name input.
     """
-    print(f"Hello {NAME}! Welcome to the Gaeilge quiz, just do your best.")
-
+    print(f"Hello {NAME}! Welcome to the Gaeilge quiz, just do your best.\n")
+    print("This is a multiple choice quiz with 10 questions, and 4 answer"
+          " options. When you are ready to answer, you can enter either 'a',"
+          " 'b', 'c', or 'd' and click the enter key to submit your answer.\n"
+          "You will be asked at the end of each question if you would like "
+          "to proceed to the next question. Click 'y' and enter for yes, or"
+          " 'n' and enter for no.")
+    
+    ready_to_begin = input(f"OK {NAME}, are you ready to try it? (y/n):")
 
 
 def main():
@@ -42,6 +50,6 @@ def main():
     Runs all program functions
     """
     enter_username()
-    start_quiz()
+
 
 main()
